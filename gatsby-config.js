01dd8b547d5a508ece41f12d131e933ec88df85d
Rob Cheseldine <robert.cheseldine@gmail.com>
2019-01-06 20:47:20 -0500

    update with icon and config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#773141`,
         theme_color: `#773141`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/face-palm-logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
