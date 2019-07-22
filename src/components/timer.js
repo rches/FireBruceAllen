1808d62efd488bf2d7261ad97ebf10039c604bd2
Rob Cheseldine <34692077+rches@users.noreply.github.com>
2019-07-22 15:29:46 -0400

    Updated dependencies

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,56 +1,56 @@
-import React, { Component } from "react";
-var moment = require('moment');
+import React, { Component } from 'react'
+var moment = require('moment')
 
 //date Allen started working for Redskins
-const bruceStartDate = new moment("2009-12-17");
+const bruceStartDate = new moment('2009-12-17')
 
 //timer logic
-class Timer extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            bruceTimer: '',
-            toggleOn: false
-            
-        };
-        this.handleClick = this.handleClick.bind(this);
+class Timer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bruceTimer: '',
+      toggleOn: false,
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-    handleClick() {
-        this.setState(state => ({
-            toggleOn: !state.toggleOn
-        }));
-    }
+  handleClick() {
+    this.setState(state => ({
+      toggleOn: !state.toggleOn,
+    }))
+  }
 
-   componentDidMount() {
-        this.intervalID = setInterval(
-            () => this.tick(), 1000
-        );
-    }
-    componentWillUnmount() {
-        clearInterval(this.intervalID);
-    }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000)
+  }
 
-    tick() {
-        let now = moment();
-        let bruceHasWorkedFor = now.diff(bruceStartDate, 'days');
-        this.setState({
-            bruceTimer: bruceHasWorkedFor
-        });
-    }
+  componentWillUnmount() {
+    clearInterval(this.intervalID)
+  }
 
-    render() {
-        return(
-            <div>
-                <button onClick={this.handleClick}>Is Bruce still employed?</button>
-                <p> 
-                    {this.state.toggleOn ? 
-                    'Bruce has worked for the Washington Redskins for ' + this.state.bruceTimer + ' days. What a bummer!':
-                     'Anything is possible - click to check!'} 
-                </p>
-            </div>
-        );
-    }
+  tick() {
+    let now = moment()
+    let bruceHasWorkedFor = now.diff(bruceStartDate, 'days')
+    this.setState({
+      bruceTimer: bruceHasWorkedFor,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Is Bruce still employed?</button>
+        <p>
+          {this.state.toggleOn
+            ? 'Bruce has worked for the Washington Redskins for ' +
+              this.state.bruceTimer +
+              ' days. What a bummer!'
+            : 'Anything is possible - click to check!'}
+        </p>
+      </div>
+    )
+  }
 }
 
 export default Timer
